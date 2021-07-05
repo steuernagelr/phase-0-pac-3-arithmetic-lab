15f97e3904875af93651310d0bb3d491f6662a55
Rielly Steuernagel <reiguy@comcast.net>
2021-07-05 18:37:30 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x, y){
+    return x + y
+}
+
+function subtract(x, y){
+    return x - y
+}
+
+function multiply(x, y){
+    return x * y
+}
+
+function divide(x, y){
+    return x / y
+}
+
+function increment(n){
+    return ++n
+}
+
+function decrement(n){
+    return --n
+}
+
+function makeInt(string){
+   return parseInt(string, 10)
+}
+
+function preserveDecimal(string){
+    return parseFloat(string)
+}
